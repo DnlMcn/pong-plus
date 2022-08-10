f9f357138cd50d36d7dfb81a708ffb795830f99f
Gusta2099 <haomakk.himself@gmail.com>
2022-08-10 11:42:32 -0300

    Add input bindings

---
+++ Assets/Scripts/PongController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PongController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    private PongControls pongControls;
+    
+
+    void Update()
+    {
+        
+    }
+}
