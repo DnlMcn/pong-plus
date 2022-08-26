735046806a42024addf3ccddc0aea4922b679bb1
Gusta2099 <haomakk.himself@gmail.com>
2022-08-26 11:25:23 -0300

    Add menu
    Feito por Douglas

---
+++ Assets/Scripts/TrocarCena.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarCena : MonoBehaviour
+{
+    [SerializeField] string nomeDaCena;
+
+   public void MudarCena()
+   {
+        SceneManager.LoadScene(name);
+   }
+
+   public void CreditoCena()
+   {
+       SceneManager.LoadScene(nomeDaCena);
+   }
+
+   public void MenuCena()
+   {
+       SceneManager.LoadScene(nomeDaCena);
+   }
+
+   public void SairCena()
+   {
+       Debug.Log("O jogo saiu");
+       Application.Quit();
+   }
+}
