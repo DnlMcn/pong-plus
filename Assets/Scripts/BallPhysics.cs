36e64e6592fc20de678d9321aa9427a1e0fbaea8
Gusta2099 <haomakk.himself@gmail.com>
2022-08-31 11:25:12 -0300

    Add starting velocity to ball
    - Also add walls to top and bottom of play area

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class BallPhysics : MonoBehaviour
 {
     [SerializeField] private float speed = 12f;
+    private int startingSide;
+    private Vector2 startingVector;
 
     void Start()
     {
-        
+        startingSide = Random.Range(-1, 2);
+        while (startingSide == 0) {startingSide = Random.Range(-1, 2);}
+        startingVector.y = Random.Range(-1, 2);
+        Debug.Log(startingSide);    
     }
 
     
     void Update()
+    {
+        startingVector = new Vector2(startingSide, startingVector.y);
+        transform.Translate(startingVector * speed * Time.deltaTime); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider) 
     {
         
     }
