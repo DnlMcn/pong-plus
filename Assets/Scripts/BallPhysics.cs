04753fc6279ef1198882e771d6d93598c804442a
Gusta2099 <haomakk.himself@gmail.com>
2022-09-01 08:40:54 -0300

    Add wall collision
    - Player collision to be fixed

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
-    [SerializeField] private float speed = 12f;
+    [SerializeField] private float speed;
     private int startingSide;
-    private Vector2 startingVector;
+    private Vector2 velocity;
 
     void Start()
     {
-        startingSide = Random.Range(-1, 2);
-        while (startingSide == 0) {startingSide = Random.Range(-1, 2);}
-        startingVector.y = Random.Range(-1, 2);
-        Debug.Log(startingSide);    
+        CalculateStartingVelocity();     
     }
 
     
     void Update()
     {
-        startingVector = new Vector2(startingSide, startingVector.y);
-        transform.Translate(startingVector * speed * Time.deltaTime); 
+        velocity = new Vector2(startingSide, velocity.y);
+        transform.Translate(velocity * speed * Time.deltaTime); 
     }
 
-    private void OnCollisionEnter2D(Collision2D collider) 
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.gameObject.tag == "Wall") { velocity.y *= -1; }
+        if (collider.gameObject.tag == "Player") { velocity.x *= -1; }
+    }
+
+    private void CalculateStartingVelocity()
+    {
+        startingSide = Random.Range(-1, 2);
+        while (startingSide == 0) {startingSide = Random.Range(-1, 2);}
+        velocity.y = Random.Range(-1, 2);
+        Debug.Log(startingSide);  
     }
 }
